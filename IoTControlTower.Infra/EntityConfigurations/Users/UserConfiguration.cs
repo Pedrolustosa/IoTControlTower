c4f1d071ba9555790f0aec7faba4de674473d671
Pedrolustosa <pedroeternalss@gmail.com>
2024-06-21 00:20:59 -0300

    feat: new logs and remoce line codes unnanecessary for builder

--- IoTControlTower.Infra/EntityConfigurations/Users/UserConfiguration.cs
+++ IoTControlTower.Infra/EntityConfigurations/Users/UserConfiguration.cs
@@ -1,21 +1,14 @@
-﻿using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using IoTControlTower.Domain.Entities;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace IoTControlTower.Infra.EntityConfigurations.Users
+namespace IoTControlTower.Infra.EntityConfigurations.Users;
+
+public class UserConfiguration() : IEntityTypeConfiguration<User>
 {
-    public class UserConfiguration(ILogger<UserConfiguration> logger) : IEntityTypeConfiguration<User>
+
+    public void Configure(EntityTypeBuilder<User> builder)
     {
-        private readonly ILogger<UserConfiguration> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-
-        public void Configure(EntityTypeBuilder<User> builder)
-        {
-            _logger.LogInformation("Configuring user entity");
-
-            builder.Property(u => u.Id).HasColumnName("UserId");
-
-            _logger.LogInformation("User entity configuration succeeded");
-        }
+        builder.Property(u => u.Id).HasColumnName("UsersId");
     }
 }
